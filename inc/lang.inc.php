71f87a84eb235458a5576d67d329fcc8f53ac968
dartar <dario.taraborelli@gmail.com>
2011-10-13 22:05:22 -0700

    Filtering out prefixes with no match

--- inc/lang.inc.php
+++ inc/lang.inc.php
@@ -33,8 +33,10 @@ echo <<<EOF
 EOF;
 
 $b = 0;
+//sort by value
 arsort($ps);
-$ps = array_filter($ps, function ($v){ return ($v>0);});
+//take top 100 non-null values
+$ps = array_slice(array_filter($ps, function ($v){ return ($v>0);}),100);
 foreach($ps as $pf => $c)
 {
 	echo "			data.setValue($b, 0, '$doi[$pf]');\n";
